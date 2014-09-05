4cba3ba97539c4b08891cf98bedba99ef9b71f15
Brenda Jin <brendajin@gmail.com>
2014-09-05 13:30:51 -0700

    first commit with examples

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo easy_install pip && sudo pip install Flask && sudo pip install requests && python proxy.py
---
+++ proxy.py
@@ -0,0 +1,28 @@
+from flask import Flask, Response, stream_with_context, request
+import requests
+import os
+
+app = Flask(__name__, static_url_path='/static')
+
+# This is a basic proxy. Clientside requests should be made to a URI starting with '/api'
+@app.route('/api/<path:uri>')
+def api(uri):
+	requestUri = os.path.join('http://api.macys.com/', uri)
+	if request.query_string: 
+		requestUri = os.path.join(requestUri, '?' + request.query_string)
+	req = requests.get(requestUri, headers={'Accept':'application/json', 'X-Macys-Webservice-Client-Id': 'neohack14'}, stream=True)
+	return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
+
+# This serves static files
+@app.route('/<path:path>')
+def router(path):
+	base = os.path.splitext(path)[0]
+	return app.send_static_file(path)
+
+# This serves the default page from static/index.html
+@app.route('/')
+def root():
+	return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+	app.run();
