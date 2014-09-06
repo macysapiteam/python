58308f677f3d470b318926d4dd58d9d47474442d
Brenda Jin <brendajin@gmail.com>
2014-09-06 11:09:28 -0700

    removed headers from ajax example bc proxy is working with python

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,6 @@
 $.ajax({
 	//select the service and URI
-	url: 'http://origin-api.macys.com/v3/catalog/reviews?productId=77589',
-	//set headers with your key and select json
-	headers: {
-		'Accept':'application/json',
-		'X-Macys-Webservice-Client-Id': 'neohack14'
-	},
+	url: '/v3/catalog/reviews?productId=77589',
 	success: function(data) {
 		//do something with your data
 		$('body').append('Look at your console in developer tools!')
